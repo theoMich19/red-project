47ad35c0a26c38214d7ccd401f4a1c6c255517e1
theoMich <michellontheo.pro@gmail.com>
2024-02-05 12:34:48 +0800

    fix(redirect): add redirect

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
@@ -10,41 +11,17 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-
-  let navigate = useNavigate();
-
-  useEffect(() => {
-    navigate('/home');
-  }, [])
-
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    // <div className="flex flex-col items-center bg-[url('app/assets/images/bg/large_1.png')] bg-cover h-full"></div>
+    <div className="flex flex-col items-center bg-gray-500 bg-cover h-full">
+      <div className="flex flex-col items-center w-full mb-[5vh]">
+        <h1 className="text-9xl font-bold mb-4" style={{ fontFamily: "Island Moments" }}>Enigmatique</h1>
+        <p className="max-w-[80vw] text-justify" >"Enigmatique" est un jeu captivant où l'objectif est de dévoiler un mot mystérieusement dissimulé. Pour ce faire, le joueur doit soumettre un mot de même longueur. Si une lettre fait partie du mot secret mais n'est pas positionnée correctement, elle s'affichera en orange. En revanche, si la lettre appartient au mot et est à la bonne place, elle s'illuminera en vert. Dans le cas contraire, elle demeurera discrètement grise. Plongez dans cette aventure ludique où la déduction et la perspicacité vous mèneront à la découverte du mot énigmatique.</p>
+      </div>
+      <Link to={"/game-day"}
+        className="bg-red-400 rounded-xl p-4 hover:bg-red-800 text-white font-bold text-xl">
+        Accès au jeu
+      </Link >
     </div>
   );
 }

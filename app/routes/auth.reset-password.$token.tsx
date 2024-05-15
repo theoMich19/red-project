2737fdc371c4ab37ea3703784d34fe0be78d26df
theoMich <michellontheo.pro@gmail.com>
2024-05-15 11:26:40 +0200

    feat(mail):create mail & forget passworf fct

--- app/routes/auth.reset-password.$token.tsx
+++ app/routes/auth.reset-password.$token.tsx
@@ -1,4 +1,4 @@
-import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { ActionFunctionArgs, LoaderFunctionArgs, json, redirect } from "@remix-run/node";
 import { Form, Link, useActionData, useLoaderData, useNavigation } from "@remix-run/react";
 import LayoutPage from "~/components/common/pageLayout";
 import { createUserSession, getUser } from "~/session.server";
@@ -7,11 +7,17 @@ import { motion } from 'framer-motion'; // Import motion
 import { useState } from "react";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
 
-export const action = async ({ request }: ActionFunctionArgs) => {
+export const action = async ({ request, params }: ActionFunctionArgs) => {
     const formData = await request.formData();
-    const remember = formData.get("remember");
+    formData.append("token", params.token as string)
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirm-password');
 
-    const apiUser = await fetch(`${process.env.REST_URL_API}/login`, {
+    if (password !== confirmPassword) {
+        return json({ errors: { message: "Les mots de passe ne correspondent pas." } }, { status: 400 })
+    }
+
+    const apiUser = await fetch(`${process.env.REST_URL_API}/reset-password`, {
         method: "post",
         headers: {
             Accept: "application/json"
@@ -20,33 +26,19 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     });
     const response = await apiUser?.json();
 
-    if (!response || response.message) {
-        return json({ errors: { message: "Email ou mot de passe invalide" } }, { status: 400 });
-    }
-
-    return createUserSession({
-        request,
-        user: response.user,
-        userId: response.user.id.toString(),
-        remember: remember === "on" ? true : false,
-        token: response.access_token,
-        redirectTo: '/game-day'
-    });
+    return redirect('/login')
 };
 
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-    return json({ user: await getUser(request) });
-};
-
-export default function Login() {
+export default function ResetPassword() {
     const [showPassword, setShowPassword] = useState(false);
-    const { user }: { user: User } = useLoaderData();
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
     const navigation = useNavigation();
     const actionData = useActionData<typeof action>();
     const loading = navigation.state === "idle" ? false : true;
 
+
     return (
-        <LayoutPage user={user}>
+        <LayoutPage >
             <div className="flex flex-col items-center bg-[url('app/assets/images/bg/fondLogin.png')] bg-cover bg-center h-full justify-center">
                 <motion.div
                     initial={{ opacity: 0, y: 50 }}
@@ -56,12 +48,12 @@ export default function Login() {
                 >
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                            Se connecter
+                            Mot de passe oublier
                         </h1>
                         <Form method="post" className="space-y-4 md:space-y-6">
                             <div>
                                 <label id="email" className="block mb-2 text-sm font-medium text-gray-900">Votre email</label>
-                                <input type="email" name="email" id="email" required className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5" />
+                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5" />
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm font-medium text-gray-900">Mot de passe</label>
@@ -80,20 +72,33 @@ export default function Login() {
                                     </div>
                                 </div>
                             </div>
+                            <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900">Confirmation</label>
+                                <div className="relative">
+                                    <input
+                                        type={showConfirmPassword ? "text" : "password"}
+                                        name="confirm-password"
+                                        id="confirm-password"
+                                        pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                        title="Le mot de passe doit contenir au moins 8 caractères, dont une majuscule, une minuscule, et un chiffre."
+                                        required
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5"
+                                    />
+                                    <div className="absolute inset-y-0 right-0 pr-3 flex items-center text-sm leading-5">
+                                        <button type="button" onClick={() => setShowConfirmPassword(!showConfirmPassword)}>
+                                            {showConfirmPassword ? <EyeOffIcon className="h-5 w-5 text-gray-700" /> : <EyeIcon className="h-5 w-5 text-gray-700" />}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
                             {actionData?.errors?.message && (
                                 <div className="pt-1 text-red-700">{actionData.errors.message}</div>
                             )}
-                            <div className="flex flex-col">
-                                <button type="submit" disabled={loading} className="w-full text-primary font-bold border-secondary border-2 hover:bg-secondary hover:bg-opacity-80 hover:text-white focus:ring-4 focus:outline-none focus:ring-primary rounded-lg text-lg px-5 py-2.5 text-center transition-colors duration-300 ease-in-out">
-                                    Se connecter
-                                </button>
-                                <Link to={"/auth/forget-password"} className="text-sm font-medium text-primary hover:underline text-right">
-                                    Mot de passe oublier ?
-                                </Link>
-                            </div>
+                            <button type="submit" disabled={loading} className="w-full text-primary font-bold border-secondary border-2 hover:bg-secondary hover:bg-opacity-80 hover:text-white focus:ring-4 focus:outline-none focus:ring-primary rounded-lg text-lg px-5 py-2.5 text-center transition-colors duration-300 ease-in-out">
+                                Sauvegarder
+                            </button>
                             <p className="text-sm font-light text-gray-500">
-                                Pas encore de compte? {" "}
-                                <Link to={"/register"} className="font-medium text-primary hover:underline">S'inscrire ici</Link>
+                                Déjà un compte ? <Link to={"/login"} className="font-medium text-primary hover:underline">Se connecter</Link>
                             </p>
                         </Form>
                     </div>

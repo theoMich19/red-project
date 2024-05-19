f6e981f896c8d7b2ff5f09d09c0a39c67879635e
theoMich <michellontheo.pro@gmail.com>
2024-05-19 23:34:34 +0200

    update(global): update train, update style global

--- app/root.tsx
+++ app/root.tsx
@@ -47,7 +47,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        {loading && <EnigmaticLoader />}
+        {!loading && <EnigmaticLoader />}
         <Analytics />
         <SpeedInsights />
         <Outlet />

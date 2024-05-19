e5d766f5af95f03d91d4b2abf6bb8604e31785f5
theoMich <michellontheo.pro@gmail.com>
2024-05-20 00:17:49 +0200

    hotfix(global): hotfix style page

--- app/routes/game-day.tsx
+++ app/routes/game-day.tsx
@@ -67,45 +67,45 @@ export default function GameDay() {
 
   return (
     <LayoutPage user={user}>
-      {isOpen && (<ModalGame setIsOpen={setIsOpen} gameStatus={gameStatus} secretWord={secretWord} />)}
-      {status ?
-        (
-          <div className="pt-[20vh]">
+      <div className="flex flex-col h-full items-center justify-center ">
+        {isOpen && (<ModalGame setIsOpen={setIsOpen} gameStatus={gameStatus} secretWord={secretWord} />)}
+        {status ?
+          (
             <GameBoard
               dicoUsed={dicoUsed}
               secretWord={secretWord}
               handChageGameStatus={handChageGameStatus}
               setIsOpen={setIsOpen}
               isRestAvaliable={false} />
-          </div>
-        ) :
-        (
-          <div className="min-w-[300px] min-h-[300px] md:min-w-[400px] md:min-h-[400px] pt-[20vh]">
-            <div className="w-full h-full p-8 backdrop-filter backdrop-blur-lg rounded-lg flex flex-col items-center justify-evenly text-white gap-4 md:text-base text-sm">
-              <div className="flex flex-col">
-                {
-                  errorSecretWord ? (
-                    <h1 className="text-lg md:text-2xl font-bold">{errorSecretWord}</h1>
-                  ) : (
-                    <>
-                      <h1 className="text-lg md:text-2xl font-bold">Le mot du jour a déjà été trouvé</h1>
-                      <span className="text-lg text-center p-2">Le mot caché était : </span>
-                      <span className="px-4 py-2 border-2 border-gray-600 border-dashed  w-fit items-center rounded-lg self-center">
-                        {secretWord}
-                      </span>
-                    </>
-                  )
-                }
+          ) :
+          (
+            <div className="min-w-[300px] min-h-[300px] md:min-w-[400px] md:min-h-[400px]">
+              <div className="w-full h-full p-8 backdrop-filter backdrop-blur-lg rounded-lg flex flex-col items-center justify-evenly text-white gap-4 md:text-base text-sm">
+                <div className="flex flex-col">
+                  {
+                    errorSecretWord ? (
+                      <h1 className="text-lg md:text-2xl font-bold">{errorSecretWord}</h1>
+                    ) : (
+                      <>
+                        <h1 className="text-lg md:text-2xl font-bold">Le mot du jour a déjà été trouvé</h1>
+                        <span className="text-lg text-center p-2">Le mot caché était : </span>
+                        <span className="px-4 py-2 border-2 border-gray-600 border-dashed  w-fit items-center rounded-lg self-center">
+                          {secretWord}
+                        </span>
+                      </>
+                    )
+                  }
 
+                </div>
+
+                <Link to={'/game-mod'}>
+                  <span className="mt-8 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-8 text-sm rounded">Jouer à un autre mode</span>
+                </Link>
               </div>
-
-              <Link to={'/game-mod'}>
-                <span className="mt-8 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-8 text-sm rounded">Jouer à un autre mode</span>
-              </Link>
             </div>
-          </div>
-        )
-      }
+          )
+        }
+      </div>
     </LayoutPage >
   );
 }
